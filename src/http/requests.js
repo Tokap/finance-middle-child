3795b19096352d60d5ed84062d3868a3c06f8da0
Patrick Allen <allenpatrick86@gmail.com>
2017-04-05 19:30:49 -0700

    continue to build out file architecture

--- src/http/requests.js
+++ src/http/requests.js
@@ -6,20 +6,6 @@ const Request   = require('request-promise');
 
 const MYSQL_GET_ONE = process.env.MYSQL_GET_ONE;
 
-
-// let options = {
-//   method: 'POST',
-//   uri: 'https://api.github.com/user/repos',
-//   headers: {
-//       'User-Agent': 'application/json'
-//   },
-//   body: {
-//     some: 'payload'
-//   },
-//   json: true // Automatically parses the JSON string in the response
-// };
-
-
 // httpsRequest :: String -> String -> Object -> HttpReply
 const httpsRequest = R.curry( (url, method, body) => {
 
