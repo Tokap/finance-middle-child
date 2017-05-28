0da91721d1ea44a30d4e01ea420d8aa61c584645
Patrick Allen <allenpatrick86@gmail.com>
2017-05-28 13:52:17 -0700

    continue to build data manipulation tools to store user data

--- src/twitter/index.js
+++ src/twitter/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const R       = require('ramda')
 const Twitter = require('twitter');
+
 const STATUS_TIMELINE = 'statuses/user_timeline';
 
 require('dotenv').config();
@@ -14,17 +16,60 @@ const makeTwitterClient = () =>
   , access_token_secret : process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
 
-// makeParams :: String -> Int -> Int -> TwitterParams
-const makeParams = (screen_name, user_id, post_count) => {
+// makeParams :: String -> Int -> TwitterParams
+const makeParams = R.curry( (screen_name, post_count) => {
   return {
     screen_name : screen_name
-  , user_id     : user_id
   , count       : post_count
   }
-}
+})
+
+// _getUserProfile :: TweetQueryReturn -> TwitterUser
+const _getUserProfile = R.compose(R.prop('user'), R.head)
+
+// _getFirstName :: TwitterUser -> String
+const _getFirstName = R.compose(
+  R.head
+, R.split(' ')
+, R.prop('name')
+)
+
+// _getLastName :: TwitterUser -> String
+const _getLastName = R.compose(
+  R.join(' ')
+, R.tail
+, R.split(' ')
+, R.prop('name')
+)
+
+// _makeProfileInsert :: TwitterUser -> InsertParameters
+const _makeProfileInsert = R.applySpec({
+  username           : R.propOr(null, 'screen_name')
+, network_id         : R.propOr(null, 'id')
+, first_name         : _getFirstName
+, last_name          : _getLastName
+, description        : R.propOr(null, 'description')
+, location           : R.propOr(null, 'location')
+, followers_count    : R.propOr(null, 'followers_count')
+, friends_count      : R.propOr(null, 'friends_count')
+, account_created_at : R.propOr(null, 'created_at')
+, profile_image_url  : R.propOr(null, 'profile_image_url')
+, verified           : R.propOr(null, 'verified')
+})
+
+// _getInsertFromProfile :: TwitterResponse -> InsertParameters
+const _getInsertFromProfile = R.compose(_makeProfileInsert, _getUserProfile)
+
+// :: String -> TwitterClient -> InsertParameters
+const getUserDetails = R.curry( (screen_name, client) => {
+  let params = makeParams(screen_name, 1)
+  return client.get(STATUS_TIMELINE, params)
+  .then( _getInsertFromProfile )
+  .then(console.log)
+})
 
 // Example Use of Twitter API Interaction:
-let my_params = makeParams('AndreVasilescu', 613, 1);
+let my_params = makeParams('AndreVasilescu', 1);
 let my_client = makeTwitterClient();
 
 my_client.get(STATUS_TIMELINE, my_params)
@@ -34,6 +79,8 @@ my_client.get(STATUS_TIMELINE, my_params)
   .catch( (error) => {
     console.log(error);
   })
+
+// getUserDetails('AndreVasilescu', my_client)
 
 // module.exports = {
 //   makeTwitterClient : makeTwitterClient
---
+++ src/twitter/type-alias.js
@@ -0,0 +1,85 @@
+// type alias TweetQueryReturn =
+// { created_at: 'Sat May 20 22:16:42 +0000 2017'
+// , id : Number
+// , id_str : String
+// , text : String
+// , truncated : Bool
+// , entities:
+//    { hashtags: [Object]
+//    , symbols: List String
+//    , user_mentions: [Object]
+//    , urls: List String
+//    , media: [Object]
+//    }
+// , extended_entities: { media: [Object] }
+// , source : String
+// , in_reply_to_status_id : null
+// , in_reply_to_status_id_str : null
+// , in_reply_to_user_id : null
+// , in_reply_to_user_id_str : null
+// , in_reply_to_screen_name : null
+// , user : TwitterUser
+// , geo : null
+// , coordinates : null
+// , place : null
+// , contributors : null
+// , retweeted_status : Object
+
+
+// type alias TwitterUser =
+// { id : Number
+// , id_str : String
+// , name : String ( Ex - 'Andrei Vasilescu' )
+// , screen_name : String
+// , location : String ( Ex - 'New York, USA' )
+// , description : String
+// , url : String
+// , entities : { description : { urls : List String } }
+// , protected : Bool
+// , followers_count : Number
+// , friends_count : Number
+// , listed_count : Number
+// , created_at : String ( Ex - 'Mon Feb 02 15:18:51 +0000 2015' )
+// , favourites_count : Number
+// , utc_offset : Number
+// , time_zone : String ( Ex - 'Pacific Time (US & Canada)' )
+// , geo_enabled : Bool
+// , verified : Bool
+// , statuses_count : Number
+// , lang : String ( Ex - 'en' )
+// , contributors_enabled : Bool
+// , is_translator : Bool
+// , is_translation_enabled : Bool
+// , profile_background_color : String ( Ex - '000000' )
+// , profile_background_image_url : String
+// , profile_background_image_url_https : String
+// , profile_background_tile : Bool
+// , profile_image_url : String
+// , profile_image_url_https : String
+// , profile_link_color : String ( Ex - '4A913C' )
+// , profile_sidebar_border_color : String ( Ex - '000000' )
+// , profile_sidebar_fill_color : String ( Ex - '000000' )
+// , profile_text_color : String ( Ex - '000000' )
+// , profile_use_background_image : Bool
+// , has_extended_profile : Bool
+// , default_profile : Bool
+// , default_profile_image : Bool
+// , following : Bool
+// , follow_request_sent : Bool
+// , notifications : Bool
+// , translator_type : String ( Ex - 'none' )
+// }
+
+// type alias InsertParameters =
+// { username : String
+// , network_id : Number
+// , first_name : String
+// , last_name : String
+// , description : String
+// , location : String
+// , followers_count : Number
+// , friends_count : Number
+// , account_created_at : String/Timestamp
+// , profile_image_url : String
+// , verified : Bool
+// }
