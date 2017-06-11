39a88ce8d9bb282efac445bf2cc8edbc5eb3f605
Patrick Allen <allenpatrick86@gmail.com>
2017-06-10 21:26:16 -0700

    add basic API features

--- src/postgres/get.js
+++ src/postgres/get.js
@@ -47,10 +47,62 @@ const getTicketBySymbol = R.curry( (knex, symbol) =>
   knex.select().from(Tables.stock_ticket).where( { symbol: symbol } )
 )
 
+const getTwitterUserByUsername = R.curry( (knex, username) => {
+  let where = `LOWER(username)= ?`
+  let params = `${R.toLower(username)}`
+
+  return knex(Tables.twitter_user).whereRaw(where, params)
+})
+
+const getTwitterUserById = R.curry( (knex, user_id) =>
+  knex(Tables.twitter_user).where({ id : user_id })
+)
+
+const getTwitterPostById = R.curry( (knex, post_id) =>
+  knex(Tables.twitter_post).where({ id : post_id })
+)
+
+const getTwitterPostsByUserId = R.curry( (knex, user_id) =>
+  knex(Tables.twitter_post).where({ twitter_user_id : user_id })
+)
+
+const getStockById = R.curry( (knex, stock_id) =>
+  knex(Tables.stock_ticket).where({ id : stock_id })
+)
+
+const getAllStocks = R.curry( (knex) =>
+  knex.select().table(Tables.stock_ticket)
+)
+
+const getStockHistoryByStockId = R.curry( (knex, stock_id) =>
+  knex(Tables.stock_pricing)
+  .where({ stock_ticket_id : stock_id })
+  .orderBy('date', 'asc')
+)
+
+const getStockHistoryBySymbol = R.curry( (knex, symbol) =>
+  knex(Tables.stock_pricing)
+  .leftJoin('stock_ticket', 'stock_ticket.id', 'stock_pricing.stock_ticket_id')
+  .where({ symbol : symbol })
+  .orderBy('date', 'asc')
+)
+
 
 module.exports = {
   getPostDetails
 , getStockTweetsById
+
+, getStockById
+, getStockHistoryByStockId
+, getStockHistoryBySymbol
+
+, getAllStocks
 , getTicketBySymbol
+
+, getTwitterPostById
+, getTwitterPostsByUserId
+
+, getTwitterUserById
+, getTwitterUserByUsername
 , getUserQueryDetails
 }
--- src/server/index.js
+++ src/server/index.js
@@ -2,39 +2,78 @@
 
 require('dotenv').config();
 
-const express       = require('express');
-const bodyParser    = require('body-parser');
-const app           = express();
+const R             = require('ramda')
+const Express       = require('express');
+const BodyParser    = require('body-parser');
+const App           = Express();
+
+const Knex          = require('../postgres/init.js')
+const PgGet         = require('../postgres/get.js')
 
 const PORT = 3300;
 
+const sendJson = R.curry( (res, body) => res.json(body) )
 // ----------------------------------------------------------------------
-// --- The Server - @TODO: Old logic in use. Update to current data. ----
+// --------------------------- The Server -------------------------------
 // ----------------------------------------------------------------------
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+App.use(BodyParser.urlencoded({ extended: true }));
+App.use(BodyParser.json());
 
-app.get('/sys/alive', (req, res) => {
-  res.send({ status: "More work? Zug zug." });
+App.get('/sys/alive', (req, res) => {
+  res.send({ status: "Server is running." });
 })
 
-app.get('/quote/:symbol', (req, res) => {
-  let symbol = req.params.symbol;
-  return symbol
-})
+App.get('/username/:username', (req, res) =>
+  PgGet.getTwitterUserByUsername(Knex, req.params.username)
+  .then(sendJson(res))
+  .catch((e) => console.log('Server Error: ', e))
+)
 
-// app.get('/price/:symbol', (req, res) => {
-//   let symbol = req.params.symbol;
-//   getQuote(symbol)
-//   .then( (quote_details) => {
-//     let quote = JSON.parse(quote_details);
-//     res.json(quote["LastPrice"])
-//   })
-// })
+App.get('/user_id/:user_id', (req, res) =>
+  PgGet.getTwitterUserById(Knex, req.params.user_id)
+  .then(sendJson(res))
+  .catch((e) => console.log('Server Error: ', e))
+)
+
+App.get('/post/:post_id', (req, res) =>
+  PgGet.getTwitterPostById(Knex, req.params.post_id)
+  .then(sendJson(res))
+  .catch((e) => console.log('Server Error: ', e))
+)
+
+App.get('/user/:user_id/posts', (req, res) =>
+  PgGet.getTwitterPostsByUserId(Knex, req.params.user_id)
+  .then(sendJson(res))
+  .catch((e) => console.log('Server Error: ', e))
+)
+
+App.get('/stock/:stock_id', (req, res) =>
+  PgGet.getStockById(Knex, req.params.stock_id)
+  .then(sendJson(res))
+  .catch((e) => console.log('Server Error: ', e))
+)
+
+App.get('/stocks', (req, res) =>
+  PgGet.getAllStocks(Knex)
+  .then(sendJson(res))
+  .catch((e) => console.log('Server Error: ', e))
+)
+
+App.get('/stock/:stock_id/history', (req, res) =>
+  PgGet.getStockHistoryByStockId(Knex, req.params.stock_id)
+  .then(sendJson(res))
+  .catch((e) => console.log('Server Error: ', e))
+)
+
+App.get('/history/:symbol', (req, res) =>
+  PgGet.getStockHistoryBySymbol(Knex, req.params.symbol)
+  .then(sendJson(res))
+  .catch((e) => console.log('Server Error: ', e))
+)
 
 const startServer = () => {
   console.log(`Listening on Port: ${PORT}`);
-  app.listen(PORT);
+  App.listen(PORT);
 }
 
 startServer();
